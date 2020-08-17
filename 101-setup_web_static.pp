046c4d8727b833b3fc34e5d055a062f1efe70457
Immaannn2222 <imennaayari@gmail.com>
2020-08-17 22:26:01 +0100

    adv

--- 101-setup_web_static.pp
+++ 101-setup_web_static.pp
@@ -1,18 +1,18 @@
 # setting up web server with puppet this time
-exec {'Nginx setup': 
+exec {'Nginx setup':
 comman => 'sudo apt-get -y update
-sudo apt-get -y install nginx
-mkdir -p /data/web_static/releases/
-mkdir -p /data/web_static/shared/
-echo "<html>
-  <head>
-  </head>
-  <body>
-    Holberton School
-  </body>
-</html>" > /data/web_static/releases/test/index.html
-sudo ln -sf /data/web_static/releases/test/ /data/web_static/current
-sudo chown -R ubuntu: /data/
+sudo apt-get -y install nginx;
+mkdir -p /data/web_static/releases/;
+mkdir -p /data/web_static/shared/;
+echo "<html>;
+  <head>;
+  </head>;
+  <body>;
+    Holberton School;
+  </body>;
+</html>" > /data/web_static/releases/test/index.html;
+sudo ln -sf /data/web_static/releases/test/ /data/web_static/current;
+sudo chown -R ubuntu: /data/;
 printf %s "server {
     listen 80;
     listen [::]:80 default_server;
@@ -25,7 +25,7 @@ printf %s "server {
     alias /data/web_static/current/;
     
     }
-}" > /etc/nginx/sites-available/default
-sudo service nginx restart'
+}" > /etc/nginx/sites-available/default;
+sudo service nginx restart;',
 provider => "shell",
 }
