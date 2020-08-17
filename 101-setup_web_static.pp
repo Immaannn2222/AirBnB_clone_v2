e3a92746897cfaddfc6ec10f780a113948a146c0
Immaannn2222 <imennaayari@gmail.com>
2020-08-17 22:21:41 +0100

    adv

---
+++ 101-setup_web_static.pp
@@ -0,0 +1,31 @@
+# setting up web server with puppet this time
+exec {'Nginx setup': 
+comman => 'sudo apt-get -y update
+sudo apt-get -y install nginx
+mkdir -p /data/web_static/releases/
+mkdir -p /data/web_static/shared/
+echo "<html>
+  <head>
+  </head>
+  <body>
+    Holberton School
+  </body>
+</html>" > /data/web_static/releases/test/index.html
+sudo ln -sf /data/web_static/releases/test/ /data/web_static/current
+sudo chown -R ubuntu: /data/
+printf %s "server {
+    listen 80;
+    listen [::]:80 default_server;
+    root   /var/www//html;
+    index index.nginx-debian.html index.nginx-debian.html;
+    location /redirect_me {
+        return 301 http://youtube.com/;
+    }
+    location /hbnb_static {
+    alias /data/web_static/current/;
+    
+    }
+}" > /etc/nginx/sites-available/default
+sudo service nginx restart'
+provider => "shell",
+}
