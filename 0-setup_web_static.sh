76f9538577e3f2d607166b33c306cc4074c4f0be
Immaannn2222 <imennaayari@gmail.com>
2020-08-21 11:43:19 +0100

    fix

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -1,30 +1,40 @@
 #!/usr/bin/env bash
 # Bash script that sets up your web servers for the deployment of web_static
-sudo apt-get -y update
-sudo apt-get -y install nginx
-mkdir -p /data/web_static/releases/
-mkdir -p /data/web_static/shared/
-echo "<html>
+
+sudo apt update
+sudo apt upgrade
+sudo apt install -y nginx
+sudo mkdir -p /data/web_static/shared/
+sudo mkdir -p /data/web_static/releases/test/
+printf %s "{
+<html>
   <head>
   </head>
   <body>
     Holberton School
   </body>
-</html>" > /data/web_static/releases/test/index.html
+</html>
+} "> /data/web_static/releases/test/index.html
 sudo ln -sf /data/web_static/releases/test/ /data/web_static/current
-sudo chown -R ubuntu: /data/
+sudo chown -hR ubuntu /data/
+sudo chgrp -hR ubuntu /data/
 printf %s "server {
-    listen 80;
+    listen 80 default_server;
     listen [::]:80 default_server;
-    root   /var/www//html;
-    index index.nginx-debian.html index.nginx-debian.html;
+    add_header X-Served-By $HOSTNAME;
+    root   /var/www/html;
+    index  index.html index.htm;
     location /redirect_me {
-        return 301 http://youtube.com/;
+        return 301 http://cuberule.com/;
     }
+    error_page 404 /404.html;
+    location /404 {
+      root /var/www/html;
+      internal;
+        }
     location /hbnb_static {
-    alias /data/web_static/current;
-    index idex.html index.htm;
-    
-    }
+        alias /data/web_static/current;
+        index index.hmtl index.htm;
+        }
 }" > /etc/nginx/sites-available/default
 sudo service nginx restart
