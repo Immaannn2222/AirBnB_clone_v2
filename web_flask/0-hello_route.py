115f10e9cb6eba64865d1d0416d0dee9e3085125
Immaannn2222 <imennaayari@gmail.com>
2020-09-01 22:04:58 +0100

    first

---
+++ web_flask/0-hello_route.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+"""flask"""
+
+
+from flask import Flask
+app = Flask(__name__)
+
+
+@app.route('/', strict_slashes=False)
+def say_hi():
+    """say hi"""
+    return 'Hello HBNB'
+
+
+if __name__ == '__main__':
+    """main"""
+    app.run(host='0.0.0.0', port=5000)
---
+++ web_flask/2-c_route.py
@@ -0,0 +1,30 @@
+#!/usr/bin/python3
+"""flask"""
+
+
+from flask import Flask
+app = Flask(__name__)
+
+
+@app.route('/', strict_slashes=False)
+def say_hi():
+    """say hi"""
+    return 'Hello HBNB'
+
+
+@app.route('/hbnb', strict_slashes=False)
+def another_one():
+    """route 2"""
+    return 'HBNB'
+
+
+@app.route('/c/<text>', strict_slashes=False)
+def router():
+    """third"""
+    modify = text.replace("_", " ")
+    return 'C {}'.format(modify)
+
+
+if __name__ == '__main__':
+    """main"""
+    app.run(host='0.0.0.0', port=5000)
