9e052d81c5e811a4c12074d426f0fada365f0625
Immaannn2222 <imennaayari@gmail.com>
2020-09-02 16:26:37 +0100

    task 6

---
+++ web_flask/6-number_odd_or_even.py
@@ -0,0 +1,59 @@
+#!/usr/bin/python3
+"""flask"""
+
+
+from flask import Flask
+from flask import render_template
+app = Flask(__name__)
+
+
+@app.route('/', strict_slashes=False)
+def say_hi():
+    """say hi"""
+    return 'Hello HBNB!'
+
+
+@app.route('/hbnb', strict_slashes=False)
+def another_one():
+    """route 2"""
+    return 'HBNB'
+
+
+@app.route("/c/<text>", strict_slashes=False)
+def c_fun(text):
+    """third"""
+    text = text.replace("_", " ")
+    return 'C {}'.format(text)
+
+
+@app.route("/python/", strict_slashes=False)
+@app.route("/python/<text>", strict_slashes=False)
+def pyth_func(text='is cool'):
+    """fourth route"""
+    text = text.replace("_", " ")
+    return 'Python {}'.format(text)
+
+
+@app.route("/number/<int:n>", strict_slashes=False)
+def is_int(n):
+    """is it an integer"""
+    return '%d is a number' % n
+
+
+@app.route('/number_template/', strict_slashes=False)
+@app.route('/number_template/<int:n>', strict_slashes=False)
+def display_page(n=None):
+    """diplays on a condition"""
+    return render_template('5-number.html', n=n)
+
+
+@app.route('/number_odd_or_even/', strict_slashes=False)
+@app.route('/number_odd_or_even/<int:n>', strict_slashes=False)
+def display_pairity(n=None):
+    """displays only if integer"""
+    return render_template("6-number_odd_or_even.html", n=n)
+
+
+if __name__ == '__main__':
+    """main"""
+    app.run(host='0.0.0.0', port=5000)
